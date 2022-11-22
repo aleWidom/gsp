e39674d1ebdebeebf25be44800d30b0208e7249f
alejandro <widomlanski@hotmail.com>
2022-11-22 17:14:02 -0300

    sprint1-v11

--- components/molecules/cards/write-word/index.tsx
+++ components/molecules/cards/write-word/index.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import { useContext } from 'react'
 import { GspContext } from '../../../../context/gsp/GspContext'
 import styles from './WriteWord.module.scss'
 
